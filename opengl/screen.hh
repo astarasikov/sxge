edaee24278c0cb884b5e56225bffb26f07deaa36
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-01-01 03:29:25 +0400

    Add abstract class for OpenGL Screen

---
+++ opengl/screen.hh
@@ -0,0 +1,24 @@
+#ifdef __SXGE_OPENGL_SCREEN_HH__
+#define __SXGE_OPENGL_SCREEN_HH__
+
+namespace sxge {
+
+class Screen {
+public:
+	typedef enum MouseButton {
+		NONE = 0,
+		LEFT = (1 << 0),
+		RIGHT = (1 << 1),
+		MIDDLE = (1 << 2),
+	} MouseButton;
+
+	virtual ~Screen();
+	virtual void keyEvent(char key, bool isDown) = 0;
+	virtual void mouseEvent(unsigned x, unsigned y, MouseButton buttons) = 0;
+	virtual void display(void) = 0;
+	virtual void reshape(unsigned width, unsigned height) = 0;
+};
+
+} //namespace sxge
+
+#endif //__SXGE_OPENGL_SCREEN_HH__
