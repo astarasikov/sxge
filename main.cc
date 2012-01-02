b221f1f2b935ec7668059ab7d7e78b63952611f1
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-01-02 18:21:19 +0400

    Make the first working drawing of a colored plane with SXGE

--- main.cc
+++ main.cc
@@ -11,32 +11,88 @@ void printGLInfo(void) {
 	auto vendor = glGetString(GL_VENDOR);
 	auto version = glGetString(GL_VERSION);
 	auto glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-	printf("OpenGL Vendor %s\n", vendor);
-	printf("OpenGL Renderer %s\n", renderer);
-	printf("OpenGL Version %s\n", version);
-	printf("GLSL Version %s\n", glslVersion);
+	info("OpenGL Vendor %s", vendor);
+	info("OpenGL Renderer %s", renderer);
+	info("OpenGL Version %s", version);
+	info("GLSL Version %s", glslVersion);
 }
 
 class TestScreen : public sxge::Screen {
 public:
+	TestScreen() : shaderProg(NULL) {
+	}
+
+	~TestScreen() {
+		if (shaderProg) {
+			delete shaderProg;
+		}
+	}
+
 	void init(void) {
 		printGLInfo();
+		shaderProg = new sxge::ShaderProgram();
+		shaderProg->addShaderFromFile(sxge::Shader::Vertex, 
+			"shaders/test.vert");
+		shaderProg->addShaderFromFile(sxge::Shader::Fragment, 
+			"shaders/test.frag");
+		shaderProg->link();
 	}
 
 	void keyEvent(char key, bool isDown) {
-		printf("%s:%c:%d\n", __func__, key, isDown);
+		dbg("%s:%c:%d", __func__, key, isDown);
 	}
 	void mouseEvent(unsigned x, unsigned y, MouseButton buttons) {
-		printf("%s:(%d,%d) %d\n", __func__, x, y, buttons);
+		dbg("%s:(%d,%d) %d", __func__, x, y, buttons);
 	}
 	void display(void) {
-		//printf("%s\n", __func__);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.0, 1.0, 0.0, 1.0);
+		glClearColor(1.0, 1.0, 1.0, 1.0);
+		shaderProg->bind();
+		drawPlane(0.5);
 	}
 	void reshape(unsigned width, unsigned height) {
-		printf("%s: %d %d", __func__, width, height);
+		dbg("%s: %d %d", __func__, width, height);
 		glViewport(0, 0, width, height);
+	}
+protected:
+	sxge::ShaderProgram *shaderProg;
+
+	void drawPlane(float size) {
+		GLfloat vertices[] = {
+			0, 0,
+			size, 0,
+			size, size,
+			0, size,
+		};
+
+		GLfloat colors[] = {
+			1, 0, 0,
+			0, 1, 0,
+			0, 0, 1,
+			0, 1, 1,
+		};
+
+		GLuint attr_pos, attr_col, mvp_mtx;
+		GLuint pid = shaderProg->programID();
+
+		attr_pos = glGetAttribLocation(pid, "position");
+		attr_col = glGetAttribLocation(pid, "color");
+		mvp_mtx = glGetUniformLocation(pid, "MVP");
+
+		auto mid = vmath::mat4f::identity();
+
+		glUniformMatrix4fv(mvp_mtx, 1, GL_FALSE, mid.getData());
+
+		glVertexAttribPointer(attr_pos, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+		glVertexAttribPointer(attr_col, 3, GL_FLOAT, GL_FALSE, 0, colors);
+
+		glEnableVertexAttribArray(attr_pos);
+		glEnableVertexAttribArray(attr_col);
+
+		glDrawArrays(GL_TRIANGLES, 0, 4);
+
+		glDisableVertexAttribArray(attr_pos);
+		glDisableVertexAttribArray(attr_col);
 	}
 };
 
