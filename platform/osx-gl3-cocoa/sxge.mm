93036d6ac07a4e168434f82af5a3cd8c83ee5b6f
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2014-09-12 07:03:58 +0400

    [platform] [osx] handle special keys

--- platform/osx-gl3-cocoa/sxge.mm
+++ platform/osx-gl3-cocoa/sxge.mm
@@ -22,12 +22,70 @@
 }
 
 -(void)keyDown:(NSEvent *)theEvent {
+	using namespace sxge;
+
 	NSString *characters = [theEvent characters];
 	if ([characters length] != 1) {
 		return;
 	}
-	screen_->keyEvent([characters characterAtIndex:0],
-		sxge::Screen::SK_None, sxge::Screen::KS_Down);
+
+	unichar rawKey = [characters characterAtIndex:0];
+	unichar key = 0;
+
+	NSUInteger flags = [theEvent modifierFlags];
+	Screen::KeyStatus ks = Screen::KeyStatus::KS_Up;
+	Screen::SpecialKey specialKey = Screen::SK_None;
+
+	if (flags & NSControlKeyMask) {
+		ks = (Screen::KeyStatus)(ks | Screen::KS_Ctrl);
+	}
+	if (flags & NSShiftKeyMask) {
+		ks = (Screen::KeyStatus)(ks | Screen::KS_Shift);
+	}
+	if (flags & NSAlternateKeyMask) {
+		ks = (Screen::KeyStatus)(ks | Screen::KS_Mod1);
+	}
+	if (flags & NSCommandKeyMask) {
+		ks = (Screen::KeyStatus)(ks | Screen::KS_Mod4);
+	}
+
+	#define CASE(fun) \
+		case NS ## fun ## FunctionKey: \
+			specialKey = Screen::SK_ ## fun; \
+			break;
+
+	switch (rawKey) {
+		case NSUpArrowFunctionKey:
+			specialKey = Screen::SK_Up;
+			break;
+		case NSDownArrowFunctionKey:
+			specialKey = Screen::SK_Down;
+			break;
+		case NSLeftArrowFunctionKey:
+			specialKey = Screen::SK_Left;
+			break;
+		case NSRightArrowFunctionKey:
+			specialKey = Screen::SK_Right;
+			break;
+		CASE(F1);
+		CASE(F2);
+		CASE(F3);
+		CASE(F4);
+		CASE(F5);
+		CASE(F6);
+		CASE(F7);
+		CASE(F8);
+		CASE(F9);
+		CASE(F10);
+		CASE(F11);
+		CASE(F12);
+		CASE(Delete);
+		default:
+			key = rawKey;
+	}
+	#undef CASE
+
+	screen_->keyEvent(key, specialKey, ks);
 }
 
 -(void)renderForTime:(CVTimeStamp)time
